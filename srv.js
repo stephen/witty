4fbbfe014a5585fc5bc3b039f069ce445b7ce68b
Stephen Wan <stephen@stephenwan.net>
2015-04-29 14:56:29 -0700

    Playing with fetching updates over time - might need to switch to actually running the job once per minute

--- srv.js
+++ srv.js
@@ -1,7 +1,11 @@
-var later   = require('later');
-var express = require('express');
-var Promise = require('bluebird');
-var request = Promise.promisify(require('request'));
+var later       = require('later');
+var express     = require('express');
+var Promise     = require('bluebird');
+var querystring = require('querystring');
+var co          = require('co');
+var moment      = require('moment');
+var debug       = require('debug')('witty');
+var request     = Promise.promisify(require('request'));
 
 var app = express();
 
@@ -20,31 +24,86 @@ var server = app.listen(3000, () => {
 var retrieveSched = later.parse.text('every minute');
 later.date.UTC();
 
-var retrieveRecentThreads = function() {
+var retrieveRecentThreads = function(argBlob) {
 
   var token = process.env.QUIP_TOKEN;
+  let qs = querystring.stringify(argBlob);
 
   return request({
-    uri: 'https://platform.quip.com/1/threads/recent',
+    uri: `https://platform.quip.com/1/threads/recent?${ qs }`,
     headers: {
       Authorization: `Bearer ${ token }`
-    }
+    },
+    json: true
   })
 };
 
 var refreshData = function() {
-  retrieveRecentThreads().spread((response, data) => {
-    if (response.statusCode < 200 && response.statusCode >= 300) {
-      // ...
+
+  debug('refreshing data');
+
+  function sleep(millis) {
+    var deferredResult = Promise.defer();
+    setTimeout(function() {
+      deferredResult.resolve();
+    }, millis);
+    return deferredResult.promise;
+  };
+
+  co(function*() {
+
+    let samples = [];
+
+    let max_updated_usec = Date.now() * 1000;//1430143200 * 1000 * 1000;
+    for (let i = 0; i < 30; i++, max_updated_usec -= (1000 * 1000 * 60 * 2)) {
+
+      yield sleep(600);
+
+      let [response, threads] = yield retrieveRecentThreads({
+        max_updated_usec: max_updated_usec,
+        count: 2
+      });
+
+      if (response.statusCode < 200 || response.statusCode >= 300) {
+        debug(`error occurred retrieving sample from ${ max_updated_usec }`);
+        debug(threads);
+      }
+
+      debug(`retrieved sample from ${ moment(max_updated_usec / 1000).format() }`);
+
+      samples.push(threads);
     }
 
-    data = JSON.parse(data);
+    return samples;
 
-    Object.keys(data).forEach((threadId) => {
-      console.log(threadId);
-    });
+  }).then((samples) => {
 
+    return samples.reduce((map, sample) => {
+
+      Object.keys(sample).forEach((key) => {
+        if (!map[key]) map[key] = {
+          count: 0,
+          title: sample[key].thread ? sample[key].thread.title : 'no title available',
+          updates: []
+        };
+
+        let updateTime = sample[key].thread.updated_usec;
+
+        if (!map[key].updates.includes(updateTime)) {
+          map[key].count += 1;
+          map[key].updates.push(updateTime);
+        }
+      });
+
+      return map;
+    }, {});
+
+  }).then((map) => {
+    console.log(map);
+  }).catch((err) => {
+    console.log(err.stack);
   });
+
 };
 
 later.setInterval(refreshData, retrieveSched);
